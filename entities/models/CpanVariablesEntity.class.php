bea1943d636cdcfa457e0c0e23a287902375b17d
Sergio Pérez <sergio.perez@albatronic.com>
2014-12-07 00:02:46 +0100

    template slider

--- entities/models/CpanVariablesEntity.class.php
+++ entities/models/CpanVariablesEntity.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Sergio Perez <sergio.perez@albatronic.com>
  * @copyright INFORMATICA ALBATRONIC SL
@@ -9,92 +10,104 @@
  * @orm:Entity(CpanVariables)
  */
 class CpanVariablesEntity extends EntityComunes {
-	/**
-	 * @orm GeneratedValue
-	 * @orm Id
-	 * @var integer
-	 * @assert NotBlank(groups="CpanVariables")
-	 */
-	protected $Id;
-	/**
-	 * @var integer
-	 * @assert NotBlank(groups="CpanVariables")
-	 */
-	protected $IdProyectosApps;
-	/**
-	 * @var string
-	 * @assert NotBlank(groups="CpanVariables")
-	 */
-	protected $Variable;
-	/**
-	 * @var string
-	 */
-	protected $Yml;
-	/**
-	 * Nombre de la conexion a la BD
-	 * @var string
-	 */
-	protected $_conectionName = '';
-	/**
-	 * Nombre de la tabla física
-	 * @var string
-	 */
-	protected $_tableName = 'CpanVariables';
-	/**
-	 * Nombre de la PrimaryKey
-	 * @var string
-	 */
-	protected $_primaryKeyName = 'Id';
-	/**
-	 * Relacion de entidades que dependen de esta
-	 * @var string
-	 */
-	protected $_parentEntities = array(
-		);
-	/**
-	 * Relacion de entidades de las que esta depende
-	 * @var string
-	 */
-	protected $_childEntities = array(
-			'ValoresSN',
-			'ValoresPrivacy',
-			'ValoresDchaIzq',
-			'ValoresChangeFreq',
-			'RequestMethods',
-			'RequestOrigins',
-			'CpanAplicaciones',
-		);
-	/**
-	 * GETTERS Y SETTERS
-	 */
-	public function setId($Id){
-		$this->Id = $Id;
-	}
-	public function getId(){
-		return $this->Id;
-	}
 
-	public function setIdProyectosApps($IdProyectosApps){
-		$this->IdProyectosApps = $IdProyectosApps;
-	}
-	public function getIdProyectosApps(){
-		return $this->IdProyectosApps;
-	}
+    /**
+     * @orm GeneratedValue
+     * @orm Id
+     * @var integer
+     * @assert NotBlank(groups="CpanVariables")
+     */
+    protected $Id;
 
-	public function setVariable($Variable){
-		$this->Variable = trim($Variable);
-	}
-	public function getVariable(){
-		return $this->Variable;
-	}
+    /**
+     * @var integer
+     * @assert NotBlank(groups="CpanVariables")
+     */
+    protected $IdProyectosApps;
 
-	public function setYml($Yml){
-		$this->Yml = trim($Yml);
-	}
-	public function getYml(){
-		return $this->Yml;
-	}
+    /**
+     * @var string
+     * @assert NotBlank(groups="CpanVariables")
+     */
+    protected $Variable;
 
-} // END class CpanVariables
+    /**
+     * @var string
+     */
+    protected $Yml;
 
-?>
+    /**
+     * Nombre de la conexion a la BD
+     * @var string
+     */
+    protected $_conectionName = '';
+
+    /**
+     * Nombre de la tabla física
+     * @var string
+     */
+    protected $_tableName = 'CpanVariables';
+
+    /**
+     * Nombre de la PrimaryKey
+     * @var string
+     */
+    protected $_primaryKeyName = 'Id';
+
+    /**
+     * Relacion de entidades que dependen de esta
+     * @var string
+     */
+    protected $_parentEntities = array(
+    );
+
+    /**
+     * Relacion de entidades de las que esta depende
+     * @var string
+     */
+    protected $_childEntities = array(
+        'ValoresSN',
+        'ValoresPrivacy',
+        'ValoresDchaIzq',
+        'ValoresChangeFreq',
+        'RequestMethods',
+        'RequestOrigins',
+        'CpanAplicaciones',
+    );
+
+    /**
+     * GETTERS Y SETTERS
+     */
+    public function setId($Id) {
+        $this->Id = $Id;
+    }
+
+    public function getId() {
+        return $this->Id;
+    }
+
+    public function setIdProyectosApps($IdProyectosApps) {
+        $this->IdProyectosApps = $IdProyectosApps;
+    }
+
+    public function getIdProyectosApps() {
+        return $this->IdProyectosApps;
+    }
+
+    public function setVariable($Variable) {
+        $this->Variable = trim($Variable);
+    }
+
+    public function getVariable() {
+        return $this->Variable;
+    }
+
+    public function setYml($Yml) {
+        $this->Yml = trim($Yml);
+    }
+
+    public function getYml() {
+        return $this->Yml;
+    }
+
+}
