66c1be1522cbb87b69ae71f844e8e803d255efd8
Sergio <sergio.perez@albatronic.com>
2015-03-08 23:46:13 +0100

    ignore

--- themes/themea/modules/Error404/Error404Controller.class.php
+++ themes/themea/modules/Error404/Error404Controller.class.php
@@ -7,7 +7,7 @@
  */
 class Error404Controller extends ControllerProject {
 
-    var $entity = "Error404";
+    var $controller = "Error404";
 
     public function IndexAction() {
         header("HTTP/1.0 404 Not Found");
